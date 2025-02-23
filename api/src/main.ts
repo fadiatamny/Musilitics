0b4f6a5bf76f3b970012c214d91adae560448c42
Fadi Atamny <fadiatamny@gmail.com>
2025-02-23 14:38:10 +0200

    cors middleware

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,6 +6,7 @@ import { config } from './config'
 import { RegisterRoutes } from './routes'
 import bodyParser from 'body-parser'
 import {
+    corsMiddleware,
     errorMiddleware,
     sessionMiddleware,
     traceMiddleware,
@@ -19,35 +20,9 @@ function setupEJS(app: Express) {
     app.set('views', path.join(__dirname, 'templates'))
 }
 
-function injectCors(app: Express) {
-    app.use((req, res, next) => {
-        const origin = req.headers.origin
-
-        if (config.allowedOrigins.includes('*')) {
-            res.setHeader('Access-Control-Allow-Origin', '*')
-        } else {
-            if (origin) {
-                if (config.allowedOrigins.includes(origin)) {
-                    res.setHeader('Access-Control-Allow-Origin', origin)
-                }
-            } else {
-                res.setHeader('Access-Control-Allow-Origin', '*')
-            }
-        }
-
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'Content-Type, Authorization'
-        )
-        res.setHeader('Access-Control-Allow-Credentials', 'true')
-        next()
-    })
-}
-
-function setupRoutes(app: Express) {
+function prepareApp(app: Express) {
     setupEJS(app)
-    injectCors(app)
+    app.use(corsMiddleware)
     app.use(bodyParser.urlencoded({ extended: true }))
     app.use(bodyParser.json())
     app.use(sessionMiddleware)
@@ -61,7 +36,7 @@ function setupRoutes(app: Express) {
 
 async function main() {
     const app: Express = express()
-    setupRoutes(app)
+    prepareApp(app)
     try {
         app.listen(config.port, () => {
             Logger.info(`Server running on port ${config.port}`)
