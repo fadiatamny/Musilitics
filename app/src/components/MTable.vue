028b0a7f711425b008ef3b5ed5fab876097ff978
Fadi Atamny <fadiatamny@gmail.com>
2025-03-22 01:18:26 +0200

    generic tables for use later

---
+++ app/src/components/MTable.vue
@@ -0,0 +1,44 @@
+<template>
+    <q-table
+        :title="title"
+        :rows="rows"
+        :columns="columns"
+        :row-key="rowKey"
+        flat
+        bordered
+    />
+</template>
+
+<script lang="ts">
+import { QTable, type QTableProps } from 'quasar'
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+    name: 'MTable',
+    components: {
+        QTable
+    },
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        rows: {
+            type: Array as () => QTableProps['rows'],
+            required: true
+        },
+        rowKey: {
+            type: String as () => QTableProps['rowKey'],
+            required: true
+        },
+        columns: {
+            type: Array as () => QTableProps['columns'],
+            required: true
+        }
+    },
+    setup() {}
+})
+</script>
+
+<style scoped lang="scss"></style>
