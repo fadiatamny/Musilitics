3e2106e84f670513ecaeac03cb389917e924ede8
Fadi Atamny <fadiatamny@gmail.com>
2025-03-22 19:09:48 +0200

    youtube utils

--- app/src/utils/youtubeUtils.ts
+++ app/src/utils/youtubeUtils.ts
@@ -2,7 +2,7 @@ import { apiClient } from "./requestClient"
 
 export const fetchYoutubeData = async () => {
     try {
-        const response = await apiClient.get('/youtube')
+        const response = await apiClient.get('/metrics/youtube')
         return response.data
     } catch (error) {
         console.error('Error fetching YouTube data:', error)
