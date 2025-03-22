a9f7bc68efcfe548191317239e76e90646eaf44d
Fadi Atamny <fadiatamny@gmail.com>
2025-03-22 04:39:15 +0200

    added interceptor to handle autto refresh

--- app/src/utils/requestClient.ts
+++ app/src/utils/requestClient.ts
@@ -1,7 +1,29 @@
 import { config } from '@/config'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 export const apiClient = axios.create({
     baseURL: `${config.backendUri}/api`,
     withCredentials: true
 })
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    async (error: AxiosError<any>) => {
+        const originalRequest = error.config!
+
+        if (
+            error.response?.status === 401 &&
+            error.response.data?.message === 'Token expired' &&
+            !originalRequest.url?.includes('/auth/spotify/refresh') // Prevent loop
+        ) {
+          debugger
+            try {
+                await apiClient.post('/auth/spotify/refresh')
+                return apiClient.request(error.config!)
+            } catch (refreshError) {
+                return Promise.reject(refreshError)
+            }
+        }
+        return Promise.reject(error)
+    }
+)
